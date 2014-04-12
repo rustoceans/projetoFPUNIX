e9c9f14406f47ec0cc739f4cd902acbc33ace48e
Alexsander <alexsander@alexsander-linux.(none)>
2014-04-12 13:35:37 -0300

    upando

---
+++ pessoas/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, HttpResponseRedirect
+from pessoas.models import Pessoa
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def pessoaListar(request):
+    pessoas = Pessoa.objects.all()[0:10]
+    # TESTE LOCAL PARA VERIFICAR SE A TABELA ESTA LISTANDO
+    #pessoas = []
+    #pessoas.append(Pessoa(nome='UNIFRAN', email='MAIL'))
+    #pessoas.append(Pessoa(nome='CRUZEIRO'))
+    return render(request, 'pessoas/listaPessoas.html', {'pessoas': pessoas})
+
+def pessoaAdicionar(request):
+    return render(request, 'pessoas/formPessoas.html')
+
+def pessoaSalvar(request):
+    if request.method == 'POST':
+        codigo = request.POST.get('codigo', '0')
+
+        try:
+            pessoa = Pessoa.objects.get(pk=codigo)
+        except:
+            pessoa = Pessoa()
+
+        pessoa.nome = request.POST.get('nome', '')
+        pessoa.email = request.POST.get('email', '')
+        pessoa.telefone = request.POST.get('telefone', '(00) 0-0000-0000')
+        pessoa.logradouro = request.POST.get('logradouro', '')
+
+        pessoa.save()
+        return HttpResponseRedirect('/pessoas/')
+
+def pessoaBuscar(request):
+    if request.method == 'POST':
+        buscar = request.POST['consultando']
+        cidadao = Pessoa.objects.filter(nome__contains=buscar)
+        print cidadao
+
+        return render(request, 'pessoas/buscaPessoas.html', {'cidadao':cidadao})
+
+        """pessoas = []
+        consultar = Pessoa.POST['contulta']
+        pessoas = """ 
+
+    
