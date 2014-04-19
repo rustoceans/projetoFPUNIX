f68379302c2657cc834d979c18dd179f1488986c
Alexsander <alexsander@alexsander-linux.(none)>
2014-04-19 14:05:56 -0300

    up

--- projetoFP/settings.py
+++ projetoFP/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
     'caixas',
     'pessoas',
     'core',
@@ -57,13 +58,25 @@ WSGI_APPLICATION = 'projetoFP.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}"""
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'd37pbhlo6h78pu',
+    'HOST': 'ec2-54-225-102-235.compute-1.amazonaws.com',
+    'PORT': 5432,
+    'USER': 'rdeocnofnmsnlf',
+    'PASSWORD': 'O7xrQo2knUlQw0KbGVOXxqGBWr'
+  }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
